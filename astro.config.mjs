14f15c3b12b9c1d9b076383115464ec6e7da0107
Kelvin Guerrero <77016142+whoskelos@users.noreply.github.com>
2025-10-24 17:39:43 +0200

    Add passthroughImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    image: {
+        service: passthroughImageService(),
+    },
 });
