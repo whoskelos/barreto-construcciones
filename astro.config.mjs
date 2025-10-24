1bbd4f031185fd9a5eb5ff412ea7fe9b30354b40
Kelvin Guerrero <77016142+whoskelos@users.noreply.github.com>
2025-10-24 18:11:07 +0200

    Add astro vercel dependecy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig, passthroughImageService } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    image: {
-        service: passthroughImageService(),
-    },
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  image: {
+      service: passthroughImageService(),
+  },
+
+  adapter: vercel(),
 });
